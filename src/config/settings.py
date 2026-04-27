7e984c6651df85ac3644dc015fc867fd4077f505
Ilyos <223318977+Ilyos-akang@users.noreply.github.com>
2026-04-28 02:33:32 +0500

    fix: update database settings to use SQLite for development

--- src/config/settings.py
+++ src/config/settings.py
@@ -83,17 +83,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myproject',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'bu_yerga_myprojectuser_parolini_yozing',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
