ccfd9165e21da0ec0c406482eb86fb0908d121fb
Ilyos <223318977+Ilyos-akang@users.noreply.github.com>
2026-04-28 02:27:33 +0500

    add:database settings:

--- src/config/settings.py
+++ src/config/settings.py
@@ -83,13 +83,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'bu_yerga_myprojectuser_parolini_yozing',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
